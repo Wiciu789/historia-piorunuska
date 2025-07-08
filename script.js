f78bcbf73e42b3c1d66dd6d5a360fb4b69452e3b
Wiciu789 <mohamedwiciu@gmail.com>
2025-07-08 23:05:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const seconds = Math.floor((diff / 1000) % 60);
 
       document.getElementById("days-together").innerText =
-        ${days} dni, ${hours}h ${minutes}m ${seconds}s;
+       `${days} dni, ${hours}h ${minutes}m ${seconds}s`;
     }, 1000);
   }
 
